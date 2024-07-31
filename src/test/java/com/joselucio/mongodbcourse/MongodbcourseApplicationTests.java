d2201b44135336a3c48fbf86ce1b2fe714c4285a
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-07-31 15:10:08 -0300

    Initial commit

---
+++ src/test/java/com/joselucio/mongodbcourse/MongodbcourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joselucio.mongodbcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbcourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
