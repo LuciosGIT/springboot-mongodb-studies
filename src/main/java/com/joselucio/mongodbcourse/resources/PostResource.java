32877237dcd402517e2940455d031db3f0395099
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-01 13:11:17 -0300

    Multiple criteria query

--- src/main/java/com/joselucio/mongodbcourse/resources/PostResource.java
+++ src/main/java/com/joselucio/mongodbcourse/resources/PostResource.java
@@ -2,6 +2,7 @@ package com.joselucio.mongodbcourse.resources;
 
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,12 +33,26 @@ public class PostResource {
 		Post obj = postService.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
 	@GetMapping(value = "/titlesearch")
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "")String text){
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "")String text,
+			@RequestParam(value = "minDate", defaultValue = "")String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "")String maxDate){
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text,min,max);
+		return ResponseEntity.ok().body(list);
+	}
+	
 	
 }
 
