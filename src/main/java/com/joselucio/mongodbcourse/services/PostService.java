b984e39d67d51c7cdda6c5e7bb7df7ced73e9903
Jose Lucio <jloliveirajunior1903@gmail.com>
2024-08-01 01:29:45 -0300

    Post findById

---
+++ src/main/java/com/joselucio/mongodbcourse/services/PostService.java
@@ -0,0 +1,26 @@
+package com.joselucio.mongodbcourse.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joselucio.mongodbcourse.domain.Post;
+
+import com.joselucio.mongodbcourse.repository.PostRepository;
+
+import com.joselucio.mongodbcourse.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id) {
+		Optional<Post> user = postRepository.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+   }	
+	
+	
+}
